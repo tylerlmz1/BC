197dac30739fb1f66b7f55c0e1c22054989b3595
Tyler Lee <lmz.1997@gmail.com>
2020-08-15 17:02:19 +0800

    refactor to cleaner code

--- calculator.sh
+++ calculator.sh
@@ -1,106 +1,82 @@
 #!/usr/bin/env bash
 
-# -------------------------------------------------------------- #
-# Bash calculator
-# -------------------------------------------------------------- #
-
-# -------------------------------------------------------------- #
 # Check if Python exists
-# -------------------------------------------------------------- #
-# Python is included in major linux distros
 # exit with error message if python is not installed
-
 if ! [ -x "$(command -v python)" ]; then
-
   echo \
     'Error: Python is not installed.
   run
-
   sudo apt install python
-
   if you are using Debian-based Linux distro.'>&2
   exit 1
-
 fi
 
 # -------------------------------------------------------------- #
 # functions
 # -------------------------------------------------------------- #
 
-function printToTerminalAndWriteToFile(){
+#function printToTerminalAndWriteToFile(){
 
-  # print answer to terminal and add newline rightafter
-  echo $answer
-  echo
+  ## print answer to terminal and add newline rightafter
+  #echo $answer
+  #echo
 
-  # print answer to log file and add newline rightafter
-  echo $answer >> ./log
-  echo >> ./log
+  ## print answer to log file and add newline rightafter
+  #echo $answer >> ./log
+  #echo >> ./log
 
-}
+#}
 
-function mathFunction(){
+function promptOpsAndDoMath(){
+  read -p "input:" input >> ./log
+  echo "$input" >> ./log
 
-  read -p "input:" input
-
-  # append input to log file
-  echo $input >> ./log
-
-  # if input contain sin/cos/tan, do trigonometry
   if [[ $input =~ sin|cos|tan ]]; then
-
-    # do trigonometry
-    # example input: sin(30), cos(30), tan(30)
     answer=$(echo "import math; print(math.$input)" | python)
-
-  # else do basic arithmetic
   else
-
-    # do basic arithmetic
-    # example input: 1+1, 2**2*34, 5.3/2, 2-4, 5%2
     answer=$(echo "print($input)" | python)
-
   fi
 
-  printToTerminalAndWriteToFile
+  echo $answer | tee -a ./log
 
 }
 
 # -------------------------------------------------------------- #
 # Welcome message
 # -------------------------------------------------------------- #
-echo -e \
-"
-\e[96m[ Calculator ]\033[0m
+#echo -e \
+#"
+#\e[96m[ Calculator ]\033[0m
 
-This calculator supports: + - * / % **, trigonometry,
-floating point numbers and operation chaining
+#This calculator supports: + - * / % **, trigonometry,
+#floating point numbers and operation chaining
 
-In addition to printing the answer to your terminal session,
-it also logs both your input and the answer into a file
-called log in your current directory
+#In addition to printing the answer to your terminal session,
+#it also logs both your input and the answer into a file
+#called log in your current directory
 
---------------
-Example inputs
---------------
-243+432.423
-52-23
-5*6
-6/3.2
-2**2
-5%2
-sin(30)
-cos(30)
-tan(30)
+#--------------
+#Example inputs
+#--------------
+#243+432.423
+#52-23
+#5*6
+#6/3.2
+#2**2
+#5%2
+#sin(30)
+#cos(30)
+#tan(30)
 
-You can also chain operations like this:
-sin(60)+2**23.3
-2+4/2-3
+#You can also chain operations like this:
+#sin(60)+2**23.3
+#2+4/2-3
 
-Try it out!
-------------------------------------------
-"
+#Try it out!
+#------------------------------------------
+#"
 
 while true; do
-  mathFunction
+  #mathFunction
+  promptOpsAndDoMath
 done
